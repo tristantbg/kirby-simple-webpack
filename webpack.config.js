35e0b3e7334f456378499610fe55934262854ddf
Tristan Bagot <tristan.bagot@gmail.com>
2018-02-23 11:29:42 +0100

    webpack modernizr

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,45 @@
 const webpack = require('webpack');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const ModernizrWebpackPlugin = require('modernizr-webpack-plugin')
 const path = require('path');
+
+const modernizrConfig = {
+  filename: 'vendor/modernizr-bundle.js',
+  'options': [
+    'setClasses'
+  ],
+  'feature-detects': [
+    "emoji",
+    "history",
+    "touchevents",
+    "video",
+    "webgl",
+    "css/animations",
+    "css/backgroundblendmode",
+    "css/columns",
+    "css/filters",
+    "css/flexbox",
+    "css/hyphens",
+    "css/mask",
+    "css/positionsticky",
+    "css/scrollbars",
+    "css/transforms",
+    "css/transforms3d",
+    "css/transitions",
+    "css/vhunit",
+    "css/vwunit",
+    "img/srcset",
+    "img/webp",
+    "storage/localstorage",
+    "storage/sessionstorage"
+  ],
+  minify: {
+    output: {
+      comments: false,
+      beautify: false
+    }
+  }
+}
 
 //Get path so every environment works
 const projectPath = path.resolve(__dirname);
@@ -15,7 +54,8 @@ const config = {
     filename: 'app.min.js'
   },
   plugins: [
-    new UglifyJsPlugin()
+    new UglifyJsPlugin(),
+    new ModernizrWebpackPlugin(modernizrConfig)
   ],
   module: {
     loaders: [
